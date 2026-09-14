8f6351aaf59ce22a5192b8482d1dea13f8e89e0a
robiulbhai2766-byte <robiulbhai2766@gmail.com>
2026-09-14 19:56:14 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Vidzora</h1>
+      <p>Create • Share • Watch • Earn</p>
+
+      <div>
+        <a href="/login">Login</a>
+        {" | "}
+        <a href="/signup">Create Account</a>
+      </div>
+    </main>
+  );
+}
